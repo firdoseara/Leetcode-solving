a7b1df516d08a7dd1f7728b3f6d5f3a23fd137d6
firdoseara <143375967+firdoseara@users.noreply.github.com>
2024-11-09 18:41:31 +0530

    Create 3133.py

---
+++ 3133.py
@@ -0,0 +1,9 @@
+n -= 1
+        base_n = base_x = 1
+        while base_n <= n:
+            if (x&base_x) == 0:
+                if n&base_n:
+                    x |= base_x
+                base_n <<= 1
+            base_x <<= 1
+        return x
